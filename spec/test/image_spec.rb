ab67e5ede8a998192fa91c492405a6bfc36fc11f
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-11-07 14:30:19 +0900

    Remove image.rb and image_spec.rb

--- spec/test/image_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe AverageHash::Image do
-  context 'with argument "sample.jpg"' do
-    subject { AverageHash::Image.new(fixture_file_path('sample.jpg')) }
-    it { is_expected.to be_test }
-  end
-end
